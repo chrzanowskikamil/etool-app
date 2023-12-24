e044ac1aa3c9b049438589dbd5d9c8a11f6171c7
Kamil <92475432+chrzanowskikamil@users.noreply.github.com>
2023-12-24 15:08:33 +0100

    feat: cleaning up boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <h1>hello world</h1>
+    <main className={styles.main}>
+      <h1>homepage</h1>
       <Link href={'/register'}>REGISTER</Link>
+      <Link href={'/dashboard'}>go to DASHBOARD</Link>
     </main>
   );
 }
