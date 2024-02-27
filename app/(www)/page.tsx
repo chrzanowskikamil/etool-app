759709bef3ca447ce83fdd782303a019d5439797
Kamil <92475432+chrzanowskikamil@users.noreply.github.com>
2024-02-27 21:02:09 +0100

    feat: add reset password form (#10)

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -2,20 +2,18 @@ import { Button } from '@/components/ui/button';
 
 export default function HomePage() {
   return (
-    <>
-      <main className='container flex items-center justify-center min-h-screen'>
-        <div className='flex-col text-center'>
-          <h2 className='text-2xl font-bold text-center'>Discover the Power of ETool</h2>
-          <p className='text-center'>Efficiently manage your repairs with our advanced features.</p>
-          <Button
-            className='mt-6'
-            variant='outline'
-            size='lg'
-            disabled>
-            Get Started
-          </Button>
-        </div>
-      </main>
-    </>
+    <main className='container flex items-center justify-center min-h-screen'>
+      <div className='flex-col text-center'>
+        <h2 className='text-2xl font-bold text-center'>Discover the Power of ETool</h2>
+        <p className='text-center'>Efficiently manage your repairs with our advanced features.</p>
+        <Button
+          className='mt-6'
+          variant='outline'
+          size='lg'
+          disabled>
+          Get Started
+        </Button>
+      </div>
+    </main>
   );
 }
