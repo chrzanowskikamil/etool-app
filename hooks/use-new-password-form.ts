ed5d4612478b342cd8d4935a96247485183deb01
Kamil <kamil-chrzanowski@gazeta.pl>
2024-04-13 20:04:53 +0200

    refactor: new-password endpoint (#20)

--- hooks/use-new-password-form.ts
+++ hooks/use-new-password-form.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
-import { ENDPOINTS, ROUTES } from '@/lib/routes';
+import { ROUTES } from '@/lib/routes';
 import { NEW_PASSWORD_DEFAULT_VALUES, NEW_PASSWORD_FORM_SCHEMA } from '@/schemas/auth';
 import { toast } from 'sonner';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
+import { createNewPassword } from '@/server/actions/user/reset-password';
 
 export function useNewPasswordForm() {
   const DELAY_ERROR = 300;
@@ -18,25 +19,20 @@ export function useNewPasswordForm() {
   });
 
   async function onSubmit(value: z.infer<typeof NEW_PASSWORD_FORM_SCHEMA>) {
+    const { password } = value;
     const verificationToken = new URLSearchParams(window.location.search).get('token');
-    try {
-      const response = await fetch(ENDPOINTS.NEW_PASSWORD, {
-        method: 'POST',
-        body: JSON.stringify({ value, verificationToken }),
-      });
 
-      const data = await response.json();
+    if (verificationToken) {
+      const { error, success, message } = await createNewPassword(password, verificationToken);
 
-      if (data.status === 200) {
-        toast.success(data.message, { description: data.description });
+      if (error) {
+        toast.error(error, { description: message });
+      }
+
+      if (success) {
+        toast.success(success, { description: message });
         router.push(ROUTES.LOGIN);
       }
-
-      if (data.status === 400) {
-        toast.error(data.message, { description: data.description });
-      }
-    } catch (error) {
-      console.log(error);
     }
   }
 
