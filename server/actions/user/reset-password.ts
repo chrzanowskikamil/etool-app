4b50e7a4715eca6ce61bcc9f88b59b7f92cf581e
Kamil <92475432+chrzanowskikamil@users.noreply.github.com>
2024-04-13 20:03:39 +0200

    refactor: new-password endpoint

--- server/actions/user/reset-password.ts
+++ server/actions/user/reset-password.ts
@@ -2,10 +2,12 @@
 
 import prisma from '@/lib/prisma';
 import { ROUTES } from '@/lib/routes';
-import { RESET_PASSWORD_FORM_SCHEMA } from '@/schemas/auth';
+import { NEW_PASSWORD_FORM_SCHEMA, RESET_PASSWORD_FORM_SCHEMA } from '@/schemas/auth';
 import { generateId } from 'lucia';
 import { TimeSpan, createDate } from 'oslo';
 import { sendEmail } from '../email/send-email';
+import { auth } from '@/lib/auth';
+import { Argon2id } from 'oslo/password';
 
 const EXPIRE_IN = new TimeSpan(2, 'h');
 
@@ -61,5 +63,46 @@ export async function sendPasswordResetLink(username: string) {
       error: 'Oops, something went wrong on the server',
       message: 'Try later again',
     };
+  }
+}
+
+export async function createNewPassword(newPassword: string, verificationToken: string) {
+  try {
+    const token = await prisma.resetPasswordToken.findUnique({
+      where: {
+        id: verificationToken,
+      },
+    });
+
+    if (!token) {
+      return { error: 'Setting a new password - failed', message: 'The token is invalid or expired' };
+    }
+
+    if (token) {
+      await prisma.resetPasswordToken.delete({
+        where: {
+          id: verificationToken,
+        },
+      });
+    }
+
+    await auth.invalidateUserSessions(token.userId);
+    const hashedPassword = await new Argon2id().hash(newPassword);
+
+    await prisma.user.update({
+      where: {
+        id: token.userId,
+      },
+      data: {
+        hashed_password: hashedPassword,
+      },
+    });
+
+    const session = await auth.createSession(token.userId, {});
+    const sessionCookie = auth.createSessionCookie(session.id);
+    const sessionCookiePlainObject = JSON.parse(JSON.stringify(sessionCookie));
+    return { success: 'Setting a new password - success', message: 'Your password has been changed - log in using your new credentials', sessionCookiePlainObject };
+  } catch (error) {
+    return { error: 'Setting a new password - failed', message: 'Oops, something went wrong on the server' };
   }
 }
