34a52d4fcf85917783351c0d039794ad6085f28e
Kamil <92475432+chrzanowskikamil@users.noreply.github.com>
2024-04-24 22:44:58 +0200

    build fix

--- server/actions/auth/validate-request.ts
+++ server/actions/auth/validate-request.ts
@@ -1,9 +1,9 @@
 'use server';
 
 import { auth } from '@/lib/lucia';
-import { Session, User } from 'lucia';
-import { cookies } from 'next/headers';
 import { cache } from 'react';
+import { cookies } from 'next/headers';
+import { Session, User } from 'lucia';
 
 export const validateRequest = cache(async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
   const sessionId = cookies().get(auth.sessionCookieName)?.value ?? null;
