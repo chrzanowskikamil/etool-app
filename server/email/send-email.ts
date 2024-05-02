1fe74c7c6e726fec3bc25d40d066c7b458767378
chrzanowskikamil <kamil-chrzanowski@gazeta.pl>
2024-05-02 14:00:35 +0200

    fix: redeploy for email notifications

--- server/email/send-email.ts
+++ server/email/send-email.ts
@@ -12,6 +12,8 @@ const ADMIN_EMAIL = 'reset-password@etool.solutions';
 export async function sendEmail(options: EmailOptions) {
   const { to, subject, text } = options;
 
+  console.log('Sending email...' + to);
+  console.log('Text: ' + text);
   const { data, error } = await resend.emails.send({
     from: ADMIN_EMAIL,
     to,
