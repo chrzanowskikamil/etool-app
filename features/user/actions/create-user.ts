4f94cf09ca2cf1876bb145eff25d927d95d3c63f
chrzanowskikamil <kamil-chrzanowski@gazeta.pl>
2024-05-17 23:19:31 +0200

    refactor: create queries for user/auth features

--- features/user/actions/create-user.ts
+++ features/user/actions/create-user.ts
@@ -1,27 +1,27 @@
-'use server'
+'use server';
 
-import { Argon2id } from "oslo/password";
-import { database } from "@/db";
-import { REGISTER_FORM_SCHEMA } from "../schemas/register-form-schema";
-import { User, generateId } from "lucia";
-import { z } from "zod";
+import { Argon2id } from 'oslo/password';
+import { REGISTER_FORM_SCHEMA } from '../schemas/register-form-schema';
+import { User, generateId } from 'lucia';
+import { z } from 'zod';
+import { createUserByCredentials } from '@/db/queries/user';
 
 export const createUser = async (credentials: z.infer<typeof REGISTER_FORM_SCHEMA>): Promise<User> => {
-    const userId = generateId(15);
-    const { firstName, lastName, username, password } = credentials;
-    const hashedPassword = await new Argon2id().hash(password);
-  
-    const userData: User = {
-      id: userId,
-      firstName,
-      lastName,
-      username,
-      hashed_password: hashedPassword,
-    };
-  
-    try {
-      return await database.user.create({ data: userData });
-    } catch (error) {
-      throw new Error(`Error in createUser function: ${error}`);
-    }
+  const userId = generateId(15);
+  const { firstName, lastName, username, password } = credentials;
+  const hashedPassword = await new Argon2id().hash(password);
+
+  const userCredentials: User = {
+    id: userId,
+    firstName,
+    lastName,
+    username,
+    hashed_password: hashedPassword,
   };
+
+  try {
+    return await createUserByCredentials(userCredentials);
+  } catch (error) {
+    throw new Error(`Error in createUser function: ${error}`);
+  }
+};
--- features/user/actions/sign-up-user.ts
+++ features/user/actions/sign-up-user.ts
@@ -1,35 +1,34 @@
-'use server'
+'use server';
 
-import { createUser } from "./create-user";
-import { createSession } from "@/lib/auth/create-session";
-import { findUser } from "./find-user";
-import { REGISTER_FORM_SCHEMA } from "../schemas/register-form-schema";
-import { z } from "zod";
+import { createUser } from './create-user';
+import { createSession } from '@/lib/auth/create-session';
+import { findUser } from './find-user';
+import { REGISTER_FORM_SCHEMA } from '../schemas/register-form-schema';
+import { z } from 'zod';
 
 export const signUpUser = async (credentials: z.infer<typeof REGISTER_FORM_SCHEMA>) => {
-    const { username } = credentials;
-  
-    try {
-      const { isUserExist } = await findUser(username);
-  
-      if (isUserExist) {
-        return {
-          error: 'Register Failed',
-          message: 'This email is already registered',
-        };
-      }
-  
-      const user = await createUser(credentials);
-      user && await createSession(user.id);
-      return {
-        success: 'Register Success',
-        message: 'You have successfully registered',
-      };
-    } catch (error) {
+  const { username } = credentials;
+
+  try {
+    const { isUserExist } = await findUser(username);
+
+    if (isUserExist) {
       return {
         error: 'Register Failed',
-        message: 'Something went wrong',
+        message: 'This email is already registered',
       };
     }
-  };
-  
+
+    const user = await createUser(credentials);
+    user && (await createSession(user.id));
+    return {
+      success: 'Register Success',
+      message: 'You have successfully registered',
+    };
+  } catch (error) {
+    return {
+      error: 'Register Failed',
+      message: 'Something went wrong',
+    };
+  }
+};
