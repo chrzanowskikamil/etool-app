698b6e0bb7f20af5c0aa45dcc68bafb9157f5995
Kamil <kamil-chrzanowski@gazeta.pl>
2024-05-22 22:16:04 +0200

    feat: add next-safe-action (#33)

--- features/user/actions/sign-in-user.ts
+++ features/user/actions/sign-in-user.ts
@@ -5,6 +5,7 @@ import { createSession } from '@/lib/auth/create-session';
 import { findUser } from './find-user';
 import { LOGIN_FORM_SCHEMA } from '@/features/user/schemas/login-form-schema';
 import { z } from 'zod';
+import { action } from '@/lib/safe-action';
 
 export const signInUser = async (credentials: z.infer<typeof LOGIN_FORM_SCHEMA>) => {
   const { username, password } = credentials;
@@ -30,4 +31,25 @@ export const signInUser = async (credentials: z.infer<typeof LOGIN_FORM_SCHEMA>)
   }
 };
 
+export const signInUser_SAFE_ACTION_FEATURE = action(LOGIN_FORM_SCHEMA, async ({ username, password }) => {
+  console.log(username, password);
+  try {
+    const { success, error, message, user, isUserExist } = await findUser(username);
 
+    if (!user) return { success, error, message, isUserExist };
+
+    const isPasswordValid = await new Argon2id().verify(user.hashed_password, password);
+
+    if (!isPasswordValid) return { success: false, error: 'Bad credentials', message: 'Wrong password', isPasswordValid };
+
+    await createSession(user.id);
+    return { success: true, error: '', message };
+  } catch (error) {
+    console.error(`Error in signIn function: ${error}`);
+    return {
+      success: false,
+      error: 'Internal server error',
+      message: 'An error occurred while signing in',
+    };
+  }
+});
--- features/user/hooks/use-login-form.ts
+++ features/user/hooks/use-login-form.ts
@@ -1,6 +1,6 @@
 import { LOGIN_DEFAULT_VALUES, LOGIN_FORM_SCHEMA } from '@/features/user/schemas/login-form-schema';
 import { toast } from 'sonner';
-import { signInUser } from '@/features/user/actions/sign-in-user';
+import { signInUser, signInUser_SAFE_ACTION_FEATURE } from '@/features/user/actions/sign-in-user';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, UseFormReturn } from 'react-hook-form';
@@ -18,25 +18,28 @@ export function useLoginForm(): { form: UseFormReturn<z.infer<typeof LOGIN_FORM_
   const { setError } = form;
 
   async function onSubmit(values: z.infer<typeof LOGIN_FORM_SCHEMA>) {
-    const { success, error, message, isPasswordValid, isUserExist } = await signInUser(values);
+    // const { success, error, message, isPasswordValid, isUserExist } = await signInUser(values);
+    const { data, serverError, validationErrors } = await signInUser_SAFE_ACTION_FEATURE(values);
 
-    if (isPasswordValid === false) {
-      setError('password', {
-        message,
-      });
-      toast.error(error, { description: message });
-    }
+    console.log(data, serverError, validationErrors);
 
-    if (isUserExist === false) {
-      setError('username', {
-        message,
-      });
-      toast.error(error, { description: message });
-    }
-
-    if (success) {
-      toast.success(success, { description: message });
-    }
+    //     if (data?.isPasswordValid === false) {
+    //       setError('password', {
+    //         data.message,
+    //       });
+    //       toast.error(error, { description: message });
+    //     }
+    //
+    //     if (data?.isUserExist === false) {
+    //       setError('username', {
+    //         data.message,
+    //       });
+    //       toast.error(error, { description: message });
+    //     }
+    //
+    //     if (data?.success) {
+    //       toast.success(data.success, { description: data.message });
+    //     }
   }
   return { form, onSubmit };
 }
