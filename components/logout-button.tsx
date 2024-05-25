826d9462021044245abc64f1c9676ea655bacec5
chrzanowskikamil <kamil-chrzanowski@gazeta.pl>
2024-05-25 19:47:44 +0200

    refactor: clean up sign out user feature

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -1,12 +1,9 @@
 'use client';
-
 import { Button } from './ui/button';
 import { signOutUser } from '@/features/user/actions/sign-out-user';
 
 export function LogoutButton() {
-  const handleLogout = async () => {
-    await signOutUser();
-  };
+  const handleLogout = async () => await signOutUser();
 
   return <Button onClick={() => handleLogout()}>Logout</Button>;
 }
