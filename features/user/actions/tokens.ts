f841ec928adcd8abc61471f886dfc60416ae692f
chrzanowskikamil <kamil-chrzanowski@gazeta.pl>
2024-06-01 22:45:27 +0200

    dummy commit

--- features/user/actions/tokens.ts
+++ features/user/actions/tokens.ts
@@ -2,10 +2,10 @@
 import { createResetPasswordToken, deleteResetPasswordTokenByUserId } from "@/db/queries/user";
 import { generateId } from "lucia";
 import { TimeSpan, createDate } from "oslo";
+import { TOKEN_ID_LENGTH } from "./utils";
 
 export const generateResetPasswordToken = async (userId: string) => {
     const TOKEN_EXPIRATION_TIME = new TimeSpan(2, 'h');
-    const TOKEN_ID_LENGTH = 40;
     const tokenId = generateId(TOKEN_ID_LENGTH);
 
     await deleteResetPasswordTokenByUserId(userId);
