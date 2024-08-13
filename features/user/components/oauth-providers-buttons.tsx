4eba0fca57c44e37d949024e7ce26275beeddac2
Kamil <kamil-chrzanowski@gazeta.pl>
2024-08-13 21:01:45 +0200

    bugfix: fixed oauth buttons styling (#59)

--- features/user/components/oauth-providers-buttons.tsx
+++ features/user/components/oauth-providers-buttons.tsx
@@ -5,19 +5,19 @@ import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons';
 
 export const OAuthProvidersButtons = () => {
   return (
-    <>
+    <div className='flex flex-col space-y-2 mt-2'>
       <OAuthButton
-        className={buttonVariants({ variant: 'secondary', size: 'lg', className: 'mt-4' })}
+        className={buttonVariants({ variant: 'secondary' })}
         icon={<GitHubLogoIcon className='w-6 h-6 mr-2' />}
         title={'Continue with Github'}
         provider={'github'}
       />
       <OAuthButton
-        className={buttonVariants({ variant: 'secondary', size: 'lg', className: 'mt-4' })}
+        className={buttonVariants({ variant: 'secondary' })}
         icon={<LinkedInLogoIcon className='w-6 h-6 mr-2' />}
         title={'Continue with LinkedIn'}
         provider={'linkedin'}
       />
-    </>
+    </div>
   );
 };
